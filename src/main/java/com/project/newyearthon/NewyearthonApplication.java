1af52763aeb18edcd5a7296257ee442470ed3fa9
윤준석(YunJunSeok) <wicaduce@gmail.com>
2025-01-18 16:25:42 +0900

    add: base repo
    base repository입니다.

---
+++ src/main/java/com/project/newyearthon/NewyearthonApplication.java
@@ -0,0 +1,13 @@
+package com.project.newyearthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewyearthonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewyearthonApplication.class, args);
+    }
+
+}
