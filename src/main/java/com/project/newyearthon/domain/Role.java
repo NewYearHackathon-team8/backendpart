2694635d33822833d40c3d3fd62e5d2ddc840f27
윤준석(YunJunSeok) <wicaduce@gmail.com>
2025-01-18 21:01:55 +0900

    add login files
    add login files

---
+++ src/main/java/com/project/newyearthon/domain/Role.java
@@ -0,0 +1,5 @@
+package com.project.newyearthon.domain;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/project/newyearthon/dto/UserSignUpDto.java
@@ -0,0 +1,12 @@
+package com.project.newyearthon.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSignUpDto {
+    private String email;
+    private String password;
+    private String phoneNumber;
+}
